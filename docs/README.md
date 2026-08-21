bb8af67b74e1e5c722b6e856b15cccb35186d2c0
Ashin Mohamed Sunil <ashinmohdsunil@gmail.com>
2026-08-22 02:28:30 +0800

    Updated the initial code to simply greet the user and exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Altair User Guide
 
 // Update the title above to match the actual product name
 
--- src/main/java/Altair.java
+++ src/main/java/Altair.java
@@ -1,11 +1,19 @@
 public class Altair {
     public static void main(String[] args) {
+        String separator = "____________________________________________________________";
         String banner = "   _____  .__   __         .__        \n"
                 + "  /  _  \\ |  | _/  |______ |__|______ \n"
                 + " /  /_\\  \\|  | \\   __\\__  \\|  \\_  __ \\\n"
                 + "/    |    \\  |__|  |  / __ \\|  ||  | \\/\n"
                 + "\\____|__  /____/|__| (____  /__||__|  \n"
-                + "        \\/                \\/          \n";
+                + "        \\/                \\/          ";
+
+        System.out.println(separator);
         System.out.println(banner);
+        System.out.println("Greetings, I am Altair.");
+        System.out.println("How may I help you?");
+        System.out.println(separator);
+        System.out.println("Goodbye. Let me know when you need me again.");
+        System.out.println(separator);
     }
 }
