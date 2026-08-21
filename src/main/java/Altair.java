0dcf7ea1be6ab05d227928f6bf996b7b8fb0bcd4
Ashin Mohamed Sunil <ashinmohdsunil@gmail.com>
2026-08-22 02:08:21 +0800

    Updated chatbot name

---
+++ src/main/java/Altair.java
@@ -0,0 +1,11 @@
+public class Altair {
+    public static void main(String[] args) {
+        String banner = "   _____  .__   __         .__        \n"
+                + "  /  _  \\ |  | _/  |______ |__|______ \n"
+                + " /  /_\\  \\|  | \\   __\\__  \\|  \\_  __ \\\n"
+                + "/    |    \\  |__|  |  / __ \\|  ||  | \\/\n"
+                + "\\____|__  /____/|__| (____  /__||__|  \n"
+                + "        \\/                \\/          \n";
+        System.out.println(banner);
+    }
+}
