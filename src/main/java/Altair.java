537db49f99917fc35003c12984e1a5a9f6d2fa1e
Ashin Mohamed Sunil <ashinmohdsunil@gmail.com>
2026-08-22 16:27:53 +0800

    Added text storage and listing

--- src/main/java/Altair.java
+++ src/main/java/Altair.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
-/**
- * Starts Altair and responds to commands entered by the user.
- */
 public class Altair {
+
+    private static final int MAX_TASKS = 100;
+
     public static void main(String[] args) {
         String separator = "____________________________________________________________";
         String banner = "   _____  .__   __         .__        \n"
@@ -20,6 +20,9 @@ public class Altair {
         System.out.println(separator);
 
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
+
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
 
@@ -31,8 +34,21 @@ public class Altair {
                 break;
             }
 
-            System.out.println("    " + command);
-            System.out.println(separator);
+            if (command.trim().equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("    " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(separator);
+            } else if (taskCount < MAX_TASKS) {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println("    added: " + command);
+                System.out.println(separator);
+            }
+
+
+
+
         }
     }
 }
